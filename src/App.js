655dc738ed111ba78be332c1f6a2685e26ac1415
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2023-07-21 00:33:49 +0600

    Ops

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import './JonySection/NavBar/Navbar'
+// import Navbar from './JonySection/NavBar/Navbar';
+// import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-
+    <div className='h-screen'>
+      {/* <Navbar></Navbar> */}
+      {/* <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='home' element={<Home></Home>}></Route>
+        <Route path='/skill' element={<Skill></Skill>}></Route>
+        <Route path='/work' element={<MyWork></MyWork>}></Route>
+        <Route path='/contract' element={<Contract></Contract>}></Route>
+      </Routes> */}
     </div>
   );
 }
